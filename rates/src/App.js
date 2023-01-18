601b9ab381138c101c8e4c3eaaf7be2261a68606
Natalia Novikova <106342111+NataliNovi@users.noreply.github.com>
2023-01-18 20:23:50 +0300

    tarifsCat

--- rates/src/App.js
+++ rates/src/App.js
@@ -1,97 +1,89 @@
 
 import CardStyled from './components/card.jsx';
-import styled from 'styled-components';
-import style from './App.module.css';
+import React, {useState} from 'react';
+import './App.css';
+//const { v4: uuidv4 } = require('uuid');
+import {v4 as uuidv4} from 'uuid';
 
 
-/*const cards = [
-  {limit: "Безлимитный 300", price: "300 руб/мес", speed: "До 10 Мбит/сек", trafic: "Объем включенного трафика не ограничен", background: "green"},
-  {limit: "Безлимитный 450", price: "450 руб/мес", speed: "До 50 Мбит/сек", trafic: "Объем включенного трафика не ограничен"},
-  {limit: "Безлимитный 550", price: "550 руб/мес", speed: "До 100 Мбит/сек", trafic: "Объем включенного трафика не ограничен"},
-  {limit: "Безлимитный 1000", price: "1000 руб/мес", speed: "До 200 Мбит/сек", trafic: "Объем включенного трафика не ограничен", background: 'red'},
-    ];*/
-  
     function App() {
+      const cardsArr = [
+        {
+          id: uuidv4(),
+          limit: "No limit",
+          price: "300",
+          speed: "10 m/s",
+          trafic: "No limit",
+          color: '_1',
+          isActive: false
+        },
+        {
+          id: uuidv4(),
+          limit: "No limit",
+          price: "400",
+          speed: "20 m/s",
+          trafic: "No limit",
+          color: '_2',
+          isActive: false
+        },
+        {
+          id: uuidv4(),
+          limit: "No limit",
+          price: "500",
+          speed: "30 m/s",
+          trafic: "No limit",
+          color: '_3',
+          isActive: false
+        },
+        {
+          id: uuidv4(),
+          limit: "No limit",
+          price: "600",
+          speed: "40 m/s",
+          trafic: "No limit",
+          color: '_4',
+          isActive: false
+        },
+      ];
 
-      const CardStyled = styled.div`
-      background: ${props => props.background};
-      height: ${props => props.height};
-      border-radius: 10px;
-      margin: 0.5rem 0.1rem;
-      width: 50vw;
-      display: flex;
-      flex-direction:column;
-     justify-content: flex-end;
-     box-shadow: ${props => props.boxshadow};
+      const [cards, setCards] = useState(cardsArr);
 
-      
-      `;
+      const handleClick = (id) => {
+       // console.log(id);
 
+        cards.forEach(el => {
+          if(el.isActive){
+            el.isActive=false
+          }
+          if(el.id === id) {
+            el.isActive = true;
+          }
+          setCards([...cards])
+      })
+
+/*setTimeout(() => {
+  console.log(cards)
+}, 1000)*/
+
+      }
+        
     return (
       
-<div className={style.App}>
+<div className={'App'}>
+  {
+    cards.map(el => {
+      return <CardStyled key = {el.id} id={el.id} limit={el.limit} price={el.price} speed = {el.speed} trafic = {el.trafic} isActive={el.isActive} color={el.color} handleClick={handleClick}/>
+     
+    })}
 
-<CardStyled background={'#1aabe9'} height={(400||4)+'px'}><div className={style.limit300}>Безлимитный 300</div>
-<div> <a className={style.price300}>&nbsp; &nbsp; &nbsp; руб</a> <a className={style.number300}>300</a> <a className={style.month300}>/мес&nbsp; &nbsp; &nbsp;     </a> </div>
-<div className={style.speed300}>До 10 Мбит/сек</div>
-<div className={style.trafic}>Объем включенного трафика не ограничен</div>
-</CardStyled>
 
-<CardStyled background={'#30bc78;'} height={(400||4)+'px'}><div className={style.limit300}>Безлимитный 450</div>
-<div> <a className={style.price450}>&nbsp; &nbsp; &nbsp; руб</a> <a className={style.number450}>450</a> <a className={style.month450}>/мес&nbsp; &nbsp; &nbsp;     </a> </div>
-<div className={style.speed450}>До 50 Мбит/сек</div>
-<div className={style.trafic}>Объем включенного трафика не ограничен</div></CardStyled>
-
-<CardStyled background={'rgb(222, 57, 57)'} height={(440||4)+'px'}boxshadow={'-5px 5px 5px rgb(107, 102, 102)'}><div className={style.limit300}>Безлимитный 550<br></br><br></br></div>
-<div> <a className={style.price550}>&nbsp; &nbsp; &nbsp; руб</a> <a className={style.number550}>550</a> <a className={style.month550}>/мес&nbsp; &nbsp; &nbsp;     </a> </div>
-<div className={style.speed450}>До 100 Мбит/сек</div>
-<div className={style.trafic}>Объем включенного трафика не ограничен</div></CardStyled>
-<CardStyled background={'rgb(49, 47, 47);'} height={(400||4)+'px'}><div className={style.limit300}>Безлимитный 1000</div>
-<div> <a className={style.price1000}>&nbsp; &nbsp; &nbsp; руб</a> <a className={style.number1000}>1000</a> <a className={style.month1000}>/мес&nbsp; &nbsp; &nbsp;     </a> </div>
-<div className={style.speed1000}>До 200 Мбит/сек</div>
-<div className={style.trafic}>Объем включенного трафика не ограничен</div></CardStyled>
-   
- </div>
+</div>
     );
   }
+    
+
+    export default App;
+
+
+    
   
-  export default App;
-
-
-  /*
-      const CardStyled = styled.div`
-      background: ${props => props.background};
-      height: ${props => props.height};
-      border-radius: 3px;
-      padding: 0.5rem 0;
-      margin: 0.5rem 1rem;
-      width: 11rem;
-      color: black;
-      border: 2px solid gray;
-      font-size: large;
-      `;
-  */
-
-/* function App() {
-
-  return (
-    <div className={styles.App}>
-<CardStyled background={'blue'} height={(200||4)+'px'}></CardStyled>
-<CardStyled background={'green'} height={(200||4)+'px'}></CardStyled>
-<CardStyled background={'red'} height={(250||4)+'px'}></CardStyled>
-<CardStyled background={'black'} height={(200||4)+'px'}></CardStyled>
-   </div>
-  );
-}
-
-export default App;*/
-
-
-/*<CardStyled text={"Привет всем!"} background={'blue'} height={(200||4)+'px'}></CardStyled>
-
-<CardStyled background={'green'} height={(200||4)+'px'} text={"PRIVET"} ></CardStyled>
-
-<CardStyled background={'red'} height={(250||4)+'px'} limit={cards.limit} price={cards.price} speed = {cards.speed} trafic = {cards.trafic}></CardStyled>
-
-<CardStyled background={'black'} height={(200||4)+'px'} limit={cards.limit} price={cards.price} speed = {cards.speed} trafic = {cards.trafic}></CardStyled>*/
-
--- rates/src/components/card.jsx
+++ rates/src/components/card.jsx
@@ -1,19 +1,43 @@
-import styles from './card.module.css';
-//import styled from 'styled-components';
+import './card.css';
+import React, {useState} from 'react';
+import classnames from 'classnames';
 
 
 function CardStyled (props) {
+    console.log(props.color)
+    console.log(props.isActive)
     //const {limit, price, speed, trafic}=props;
-    //console.log(props);
 
+    /*let classForCard = "card";
+    classForCard += ' card' + props.color;
 
+    if (props.isActive) {
+        classForCard += '  card_active';
+    }*/
+    
+    
+   let classForCard = classnames ({
+        'card' : true,
+        'color': props.color,
+        'isActive': props.isActive
+       })
+
+       console.log(classForCard)
+
+    const [id, setId] = useState(props.id);
+
+//паша рома макар паша мама папа
+   
+   
     return (
-        <div>
-            <div className={styles.limit}>{props.limit}</div>
-            <div className={styles.price}>{props.price}</div>
-            <div className={styles.speed}>{props.speed}</div>
-            <div className={styles.trafic}>{props.trafic}</div>
+        <div className={classForCard} onClick={() => props.handleClick(props.id)}>
+            <div className='limit'>{props.limit}</div>
+            <div className='price'>{props.price}
+            <div className='speed'>{props.speed}</div>
+            <div className='trafic'>{props.trafic}</div>
+
         </div>
+       </div>
     );
 }
 export default CardStyled;
